74010b97036e5927e8a7d1a31d3c03549b12284c
Eitan81 <eitangambit@gmail.com>
2020-05-11 19:29:32 +0300

    Add Weather data

--- src/utils/forecast.js
+++ src/utils/forecast.js
@@ -9,7 +9,7 @@ const forecast = (latitude, longtiude, callback) => {
         } else if (body.error) {
             callback('Unable to find location. Try another search', undefined)
         } else {
-            callback(undefined, body.current.weather_descriptions[0] + ". It is currently " + body.current.temperature + " degress out. It feels like " + body.current.feelslike + " degrees out."
+            callback(undefined, body.current.weather_descriptions[0] + ". It is currently " + body.current.temperature + " degress out. It feels like " + body.current.feelslike + " degrees out." + " With " + body.current.wind_dir + " " + body.current.wind_speed + "kph wind."
             )
         }
     })
